887cf178ef26db8f45236db33ff87681fe028928
AlejandroVargas <32745553+eiviertest@users.noreply.github.com>
2021-08-03 06:43:29 -0500

    Comparar password

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -4,6 +4,9 @@ class Utils {
         const salt = await bycript.genSaltSync(10);
         return await bycript.hashSync(password, salt);
     }
+    public async checkPassword(password: string, encryptedPassword: string):Promise<boolean> {
+        return await bycript.compare(password, encryptedPassword);
+    }
 }
 
 export const utils = new Utils();
