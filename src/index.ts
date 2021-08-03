acee23fe3fc88ec26a172900f0d8df351d2562c3
BrandonLeon05 <56412313+BrandonLeon05@users.noreply.github.com>
2021-08-03 00:11:07 -0500

    Configuracion del server y estructuracion del proyecto

---
+++ src/index.ts
@@ -0,0 +1,40 @@
+import express, { Application } from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
+
+
+class Server{
+
+    public app:Application; 
+
+    constructor(){
+        this.app = express();
+        this.config();
+        this.routes();
+    }
+
+    //Configuracion del servidor
+    config(): void{
+        this.app.set("port", process.env.PORT || 3000);
+        this.app.use(morgan("dev"));
+        this.app.use(cors());
+        this.app.use(express.json());
+        this.app.use(express.urlencoded({extended: false}));
+    }
+
+    //Rutas para el APIRest
+    routes(): void{
+        //this.app.use('/', indexRoutes);
+        //this.app.use('/auth', authRoutes);
+    }
+
+    //Inicialización del servidor
+    start(): void{
+        this.app.listen(this.app.get('port'), () => {
+            console.log("Server on port", this.app.get('port'));
+        });
+    }
+}
+
+const server = new Server();
+server.start();
