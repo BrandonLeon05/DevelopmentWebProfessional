08b90496170189295eab944cd1959c021dc285dd
AlejandroVargas <32745553+eiviertest@users.noreply.github.com>
2021-08-03 06:30:21 -0500

    Creacion Utils

--- src/config/keys.ts
+++ src/config/keys.ts
@@ -1,7 +1,7 @@
 export default {
     database: {
         host: 'localhost',
-        port: 3306,
+        port: 33066,
         user: 'root',
         password: '',
         database: 'sistema_departamentos'
