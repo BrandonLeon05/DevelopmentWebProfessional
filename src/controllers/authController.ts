b12164cdbcb9cb6971baa559dd308183e7a7ad87
AlejandroVargas <32745553+eiviertest@users.noreply.github.com>
2021-08-03 07:55:37 -0500

    Verificar datos enviados para Login

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,9 +1,23 @@
 import { Request, Response } from 'express';
+import { dao } from '../dao/authDAO';
+import { utils } from '../utils/utils';
+
 class AuthController {
-    public login(req: Request, res: Response) {
+    public async login(req: Request, res: Response) {
         const { usuario, password } = req.body;
         if (usuario == null || password == null)
             return res.status(400).json({message : "Los datos no pueden ser nulos"});
+        
+        const users = await dao.getUsuer(usuario);
+        if (users.length <= 0)
+            return res.status(400).json({message : "El usuario no existe"});
+        for (let user of users) {
+            if(await utils.checkPassword(password, user.password)){
+
+            } else {
+                return res.status(400).json({message : "La constraseña es incorrecta"});
+            }
+        }
     }
 }
 
--- src/dao/authDAO.ts
+++ src/dao/authDAO.ts
@@ -1,5 +1,11 @@
+import pool from '../database/database';
 class AuthDAO {
-
+    public async getUsuer(usuario: string){
+        const result = await pool.then(async (connection) => {
+            return await connection.query("Select * FROM usuario where usuario = ?", [usuario]);
+        });
+        return result;
+    }
 }
 
 export const dao = new AuthDAO();
