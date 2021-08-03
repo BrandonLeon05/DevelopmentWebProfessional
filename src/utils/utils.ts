20a0b5c3fa6a12ebbed3a13beb5cb4b18b8a3bde
AlejandroVargas <32745553+eiviertest@users.noreply.github.com>
2021-08-03 06:37:53 -0500

    Encriptacion de password

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,5 +1,9 @@
+import bycript from "bcryptjs"
 class Utils {
-
+    public async hashPassword(password: string): Promise<string> {
+        const salt = await bycript.genSaltSync(10);
+        return await bycript.hashSync(password, salt);
+    }
 }
 
 export const utils = new Utils();
