2b8db4cd35f67a5342aa96706f0b5436e30f9b13
BrandonLeon05 <56412313+BrandonLeon05@users.noreply.github.com>
2021-08-03 20:15:15 -0500

    Implementación de tokens en el usuario

--- src/dao/authDAO.ts
+++ src/dao/authDAO.ts
@@ -1,8 +1,8 @@
 import pool from '../database/database';
 class AuthDAO {
-    public async getUsuer(usuario: string){
+    public async getUser(usuario: string){
         const result = await pool.then(async (connection) => {
-            return await connection.query("Select * FROM usuario where usuario = ?", [usuario]);
+            return await connection.query("Select * FROM usuario where username = ?", [usuario]);
         });
         return result;
     }
